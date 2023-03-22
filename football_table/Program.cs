b893c910753c71ab655bc3e9bc6eeac5dbd864f2
David <doro.hd@skiff.com>
2023-03-22 10:37:31 +0100

    Create main entry

--- football_table/Program.cs
+++ football_table/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Football
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            
+        }
+    }
+
+   
+    public class T
+    {
+        public void Testing()
+        {
+
+        }
+    }
+    
+}
