afac6d0289e2701682325815fa9105380c90d881
gulernurdonmez <gule0153@stud.kea.dk>
2023-03-28 13:09:31 +0200

    can print from csv now

--- football_table/File.cs
+++ football_table/File.cs
@@ -4,7 +4,31 @@ namespace Football.File
     {
         public List<List<string>> Read(string path)
         {
-            return new List<List<string>>();
+            var lines = new List<List<string>>();
+            using (var reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var columns = line.Split(',');
+                    var columnList = new List<string>(columns);
+                    lines.Add(columnList);
+                }
+            }
+            return lines;
+        }
+
+        public void format(List<List<string>> lines)
+        {
+
+            foreach (var line in lines)
+            {
+                foreach (var column in line)
+                {
+                    Console.Write(column + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- football_table/Program.cs
+++ football_table/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Football
+﻿using Football.File;
+
+namespace Football
 {
     public class Program
     {
         public static void Main(String[] args)
         {
+            FileAssistant fileAssistant = new FileAssistant();
+            List<List<string>> list = fileAssistant.Read("../../KEA-CS-FOOTBALL/csv files/Rounds/Round-1.csv");
+
+            fileAssistant.format(list);
             
         }
     }
